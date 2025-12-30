9f3fb1ea42a36368bec64786a6e38de9d7bc880a
Razvan Vlad Pop <razvanvladpop@gmail.com>
2025-12-30 02:58:09 +0200

    video content

--- src/elements/elements-video/index.jsx
+++ src/elements/elements-video/index.jsx
@@ -2,6 +2,11 @@ import React from "react";
 // internal
 import ElementBreadcrumb from "@components/common/breadcrumb/element-breadcrumb";
 import ElVideoStyleOne from "./el-video-style-1";
+import incentiveEgyptThumbnail from "@assets/img/video/thumbnail-INCENTIVE-EGYPT.jpg";
+import prezentareCandidatThumbnail from "@assets/img/video/prezentare-candidat.jpg";
+import euSuntCiprianThumbnail from "@assets/img/video/eu-sunt-ciprian-ciceu.jpg";
+import historyBehindThumbnail from "@assets/img/video/Ciceu Ciprian - History behind.jpg";
+import mainetDubaiThumbnail from "@assets/img/video/Mainet First Incentive in Dubai.jpg";
 
 
 const ElementsVideoMain = () => {
@@ -12,16 +17,24 @@ const ElementsVideoMain = () => {
       {/* breadcrumb end */}
 
       {/* video one start */}
-      <ElVideoStyleOne videoId="IPYr8a3LgWs" />
+      <ElVideoStyleOne videoId="IPYr8a3LgWs" thumbnail={prezentareCandidatThumbnail} />
       {/* video one end */}
 
       {/* video two start */}
-      <ElVideoStyleOne videoId="aiEKoEc7P2Y" />
+      <ElVideoStyleOne videoId="aiEKoEc7P2Y" thumbnail={incentiveEgyptThumbnail} />
       {/* video two end */}
 
       {/* video three start */}
-      <ElVideoStyleOne videoId="IPYr8a3LgWs" />
+      <ElVideoStyleOne videoId="42VkeRLruqw" thumbnail={euSuntCiprianThumbnail} />
       {/* video three end */}
+
+      {/* video four start */}
+      <ElVideoStyleOne videoId="4XQLNYJc2xQ" thumbnail={historyBehindThumbnail} />
+      {/* video four end */}
+
+      {/* video five start */}
+      <ElVideoStyleOne videoId="jMdjc-ynPVw" thumbnail={mainetDubaiThumbnail} />
+      {/* video five end */}
     </React.Fragment>
   );
 };
